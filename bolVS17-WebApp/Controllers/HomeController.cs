deedddb632aafaed65786b460ad7beb35c1eb87b
Mike Benkovich <mike@benko.com>
2017-04-08 13:53:23 -0500

    Add WebAPI and Docker support

--- bolVS17-WebApp/Controllers/HomeController.cs
+++ bolVS17-WebApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
 
 namespace bolVS17_WebApp.Controllers
 {
@@ -13,11 +14,19 @@ namespace bolVS17_WebApp.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> About()
         {
             ViewData["Message"] = "Your application description page.";
 
+            var client = new HttpClient();
+
+            var response = await client.GetAsync(MySettings.API_URL + "/api/values");
+
+            var json = response.Content.ReadAsStringAsync();
+
+            ViewData["Message"] = $"Called the WebAPI and got {json.Result} as a result";
             return View();
+
         }
 
         public IActionResult Contact()
